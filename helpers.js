4c5435c9f7b1b73f93314442b13de7bd24aa28bf
Fiona Brockner <fbrockner@gmail.com>
2023-11-14 17:34:10 -0500

    Added create and get user, pet, and comments

---
+++ helpers.js
@@ -0,0 +1,33 @@
+// You can add and export any helper functions you want here - if you aren't using any, then you can just leave this file as is
+// You can add and export any helper functions you want here - if you aren't using any, then you can just leave this file as is
+import {ObjectId} from 'mongodb';
+import validateDate from "validate-date";
+import moment from "moment";
+// ! this is straight copied from professors git hub
+
+
+let exportedMethods = {
+  checkId(id, varName) {
+    if (!id) throw `Error: You must provide a ${varName}`;
+    if (typeof id !== 'string') throw `Error:${varName} must be a string`;
+    id = id.trim();
+    if (id.length === 0)
+      throw `Error: ${varName} cannot be an empty string or just spaces`;
+    if (!ObjectId.isValid(id)) throw `Error: ${varName} invalid object ID`;
+    return id;
+  },
+
+  checkString(strVal, varName) {
+    if (!strVal) throw `Error: You must supply a ${varName}!`;
+    if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
+    strVal = strVal.trim();
+    if (strVal.length === 0)
+      throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+    if (!isNaN(strVal))
+      throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+    return strVal;
+  }
+
+};
+
+export default exportedMethods;
