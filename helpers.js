71da425769390341da674c7841df577249264a18
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-03 16:23:23 -0500

    added validation method for id

--- helpers.js
+++ helpers.js
@@ -67,6 +67,16 @@ let exportedMethods = {
     else 
       throw ("Error: invalid adoption status");
   },
+
+   checkId(id, varName) {
+    if (!id) throw `Error: You must provide a ${varName}`;
+    if (typeof id !== 'string') throw `Error:${varName} must be a string`;
+    id = id.trim();
+    if (id.length === 0)
+      throw `Error: ${varName} cannot be an empty string or just spaces`;
+    if (!ObjectId.isValid(id)) throw `Error: ${varName} invalid object ID`;
+    return id;
+  },
 };
 
 export default exportedMethods;
