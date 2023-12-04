d9e8d99d81b1a5daf6b7a5f5f07c0ae369e2ee4b
Fiona Brockner <fbrockner@gmail.com>
2023-12-03 22:27:04 -0500

    fixed check id fn

--- helpers.js
+++ helpers.js
@@ -2,11 +2,7 @@ import {ObjectId} from 'mongodb';
 
 let exportedMethods = {
   checkId(id, varName) {
-    if (!id) throw `Error: You must provide a ${varName}`;
-    if (typeof id !== 'string') throw `Error:${varName} must be a string`;
-    id = id.trim();
-    if (id.length === 0)
-      throw `Error: ${varName} cannot be an empty string or just spaces`;
+    if (!id) throw `Error: You must provide a id for ${varName}`;
     if (!ObjectId.isValid(id)) throw `Error: ${varName} invalid object ID`;
     return id;
   },
