5b4f8f7b60392642e5f7ef64de9ab2e4302b5cf1
Fiona Brockner <fbrockner@gmail.com>
2023-12-07 14:49:23 -0500

    fixed obj id errors

--- data/pets.js
+++ data/pets.js
@@ -61,7 +61,7 @@ const exportedMethods = {
   async getPetById(id) {
     id = helpers.checkId(id, "pet id");
     const petsCollection = await pets();
-    const user = await petsCollection.findOne({ _id: id });
+    const user = await petsCollection.findOne({ _id: new ObjectId(id) });
     if (!user) throw "Error: no pet with that id exist";
 
     return user;
@@ -70,7 +70,7 @@ const exportedMethods = {
   async getPetByCreator(id) {
     id = helpers.checkId(id, "creator id");
     const petsCollection = await pets();
-    const pet = await petsCollection.findOne({ creatorId: id });
+    const pet = await petsCollection.findOne({ creatorId: new ObjectId(id) });
     if (!pet) throw "Error: no pet with that id exist";
 
     return pet;
@@ -90,7 +90,7 @@ const exportedMethods = {
     const petsCollection = await pets();
 
     const updatedInfo = await petsCollection.updateOne(
-      { _id: petId },
+      { _id: new ObjectId(petId) },
       { $push: { comments: newComment } }
     );
 
@@ -139,7 +139,7 @@ const exportedMethods = {
     };
 
     const updatedInfo = await petsCollection.updateOne(
-      { _id: id },
+      { _id: new ObjectId(id) },
       { $set: setPet }
     );
 
