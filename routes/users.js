6341ff91bdbe773638000f5bcef52e5403c1036a
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-08 12:50:44 -0500

    implemented basic login route

--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,52 @@ router
         res.status(500).send("Internal Server Error");
       }
     } catch (error) {
-      res.status(400).render("register/register", { error: error.message });
+      res.status(400).render("users/register", { error: error.message });
+    }
+  });
+
+router
+  .route("/login")
+  .get(async (req, res) => {
+    res.render("users/login", { title: "Login Page" });
+  })
+  .post(async (req, res) => {
+    console.log("inside login post route")
+    const { emailAddressInput, passwordInput } = req.body;
+
+    const emailAddress = helpers.checkEmail(emailAddressInput);
+    const trimmedPassword = passwordInput.trim();
+
+    if (!emailAddress || !trimmedPassword) {
+      return res
+        .status(400)
+        .render("login", { error: "Email address and password are required" });
+    }
+
+    try {
+      const user = await userData.loginUser(
+        emailAddress.toLowerCase(),
+        trimmedPassword
+      );
+
+      if (user) {
+        res.cookie("AuthState", true);
+
+        req.session.user = {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          emailAddress: user.email,
+        };
+
+        res.redirect("/pets");
+      } else {
+        res
+          .status(400)
+          .render("users/login", { error: "Invalid email address and/or password" });
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(400).render("users/login", { error: error.message });
     }
   });
 
