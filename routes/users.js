bf20e120d407702c49041da39fbaf99f9be59ca0
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-17 10:31:39 -0500

    fix: reference error for invalid email

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = Router();
 import isAuthenticated from "../middleware.js";
 import { petData, userData } from "../data/index.js";
 import helpers from "../helpers.js";
-import xss from 'xss';
+import xss from "xss";
 
 router.get("/", isAuthenticated, (req, res) => {
   return res.redirect("/pets");
@@ -12,10 +12,10 @@ router.get("/", isAuthenticated, (req, res) => {
 router
   .route("/register")
   .get(async (req, res) => {
-    res.render("users/signup", { title: "Sign Up" ,  layout: "account" });
+    res.render("users/signup", { title: "Sign Up", layout: "account" });
   })
   .post(async (req, res) => {
-    try{
+    try {
       const {
         firstNameInput,
         lastNameInput,
@@ -27,26 +27,27 @@ router
       const firstName = helpers.checkString(xss(firstNameInput), "first name");
       const lastName = helpers.checkString(xss(lastNameInput), "last name");
       const emailAddress = helpers.checkEmail(xss(emailAddressInput));
-      const passwordReg = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[~`!@#$%^&*()\-_=+{}[\]:;"'<>,.?/|\\]).{8,}$/;
+      const passwordReg =
+        /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[~`!@#$%^&*()\-_=+{}[\]:;"'<>,.?/|\\]).{8,}$/;
 
-      if (passwordInput.length === 0 || /\s/.test(passwordInput) || !passwordReg.test(passwordInput)) {
+      if (
+        passwordInput.length === 0 ||
+        /\s/.test(passwordInput) ||
+        !passwordReg.test(passwordInput)
+      ) {
         return res.status(400).render("users/signup", {
           error: "Please provide valid password",
-          layout: "account" 
+          layout: "account",
         });
       }
 
       if (passwordInput !== confirmPasswordInput) {
         return res.status(400).render("users/signup", {
           error: "Password and confirmPassword do not match",
-          layout: "account" 
+          layout: "account",
         });
       }
-    }catch(error){
-      res.status(400).render("users/signup", { error: error, layout: "account" });
-    }
 
-    try {
       const result = await userData.createUser(
         xss(firstName),
         xss(lastName),
@@ -60,32 +61,31 @@ router
         res.status(500).send("Internal Server Error");
       }
     } catch (error) {
-      res.status(400).render("users/signup", { error: error, layout: "account" });
+      res
+        .status(400)
+        .render("users/signup", { error: error, layout: "account" });
     }
   });
 
 router
   .route("/login")
   .get(async (req, res) => {
-    res.render("users/signin", { title: "Sign In" ,  layout: "account" });
+    res.render("users/signin", { title: "Sign In", layout: "account" });
   })
   .post(async (req, res) => {
-    try{
+    try {
       const { emailAddressInput, passwordInput } = req.body;
 
       const emailAddress = helpers.checkEmail(xss(emailAddressInput));
       const trimmedPassword = xss(passwordInput.trim());
 
       if (!emailAddress || !trimmedPassword) {
-        return res
-          .status(400)
-          .render("signin", { error: "Email address and password are required",  layout: "account"  });
+        return res.status(400).render("signin", {
+          error: "Email address and password are required",
+          layout: "account",
+        });
       }
-    } catch(error) {
-      res.status(400).render("users/signin", { error: error ,  layout: "account" });
-    }
 
-    try {
       const user = await userData.loginUser(
         emailAddress.toLowerCase(),
         trimmedPassword
@@ -98,7 +98,7 @@ router
           firstName: xss(user.firstName),
           lastName: xss(user.lastName),
           emailAddress: xss(user.emailAddress),
-          favoritePets: (user.favoritePets)
+          favoritePets: user.favoritePets,
         };
         //console.log("user id: " + req.session.user.userId)
 
@@ -106,26 +106,29 @@ router
       } else {
         res.status(400).render("users/signin", {
           error: "Invalid email address and/or password",
-          layout: "account" 
+          layout: "account",
         });
       }
     } catch (error) {
       console.log(error);
-      res.status(400).render("users/signin", { error: error ,  layout: "account" });
+      res
+        .status(400)
+        .render("users/signin", { error: error, layout: "account" });
     }
   });
 
 router.route("/logout").get(async (req, res) => {
   if (req.session.user) {
     req.session.destroy();
-    res.status(200).render("users/signin", { title: "Sign In" ,  layout: "account" });
+    res
+      .status(200)
+      .render("users/signin", { title: "Sign In", layout: "account" });
   } else {
     res.redirect("/login");
   }
 });
 
 router.route("/addToFavourites/:petId").get(async (req, res) => {
-
   const petId = helpers.checkId(req.params.petId, "pet id");
   const userId = helpers.checkId(req.session.user.id, "user id");
 
@@ -133,13 +136,13 @@ router.route("/addToFavourites/:petId").get(async (req, res) => {
     const result = await userData.addFavoritePet(petId, userId);
 
     if (result) {
-      res.redirect("/pets/"+req.params.petId);
+      res.redirect("/pets/" + req.params.petId);
     } else {
       res.status(500).send("Internal Server Error");
     }
   } catch (error) {
     console.log(error.message);
-    res.redirect("/pets/"+req.params.petId);
+    res.redirect("/pets/" + req.params.petId);
   }
 });
 
@@ -153,26 +156,32 @@ router.route("/removeFromFavourites/:petId").get(async (req, res) => {
     //console.log("req session", req.session.user)
 
     if (result) {
-      res.redirect("/pets/"+req.params.petId);
+      res.redirect("/pets/" + req.params.petId);
     } else {
       res.status(500).send("Internal Server Error");
     }
   } catch (error) {
     console.log(error);
-    res.redirect("/pets/"+req.params.petId);
+    res.redirect("/pets/" + req.params.petId);
   }
 });
 
 router.route("/about").get(async (req, res) => {
-  res.render("pages/about", { title: "About | Pawdoption", user:req.session.user});
+  res.render("pages/about", {
+    title: "About | Pawdoption",
+    user: req.session.user,
+  });
 });
 
 router.route("/profile").get(async (req, res) => {
   if (req.session.user) {
     const name = `${req.session.user.firstName} ${req.session.user.lastName}`;
-    res.render("users/profile", { title: `${name} | Pawdoption`, user: req.session.user});
+    res.render("users/profile", {
+      title: `${name} | Pawdoption`,
+      user: req.session.user,
+    });
   } else {
-    res.redirect("/pets")
+    res.redirect("/pets");
   }
 });
 
